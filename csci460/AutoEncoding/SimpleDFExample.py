4cb8e71e65afd36ed26e8711f7e748dcec8d82e1
R. Paul Wiegand <rpwiegand@gmail.com>
2021-03-01 07:08:02 -0500

    Updating changes to autoencoding example

--- csci460/AutoEncoding/SimpleDFExample.py
+++ csci460/AutoEncoding/SimpleDFExample.py
@@ -10,115 +10,182 @@ import tensorflow as tf  # Ignore the warning
 import csci460.Utils.DataGenerators as dg
 import csci460.Utils.PlottingAndDisplay as plotting
 
+def BuildEncoder(latentDim, imageWidth, imageHeight):
+    inputLayer = tf.keras.layers.Input(shape=(imageWidth, imageHeight, 3))
+    encLayer1 = tf.keras.layers.Conv2D(100, (3, 3),\
+                                      kernel_regularizer=tf.keras.regularizers.l1(0.01),\
+                                      activity_regularizer=tf.keras.regularizers.l2(0.01),\
+                                      activation="relu")
+    encLayer2 = tf.keras.layers.MaxPooling2D((2, 2))
+    #encLayer3 = tf.keras.layers.Conv2D(100, (3, 3), activation="relu")
+    #encLayer4 = tf.keras.layers.MaxPooling2D((4, 4))
+    encFlatten = tf.keras.layers.Flatten()
+    encDropout = tf.keras.layers.Dropout(0.05)
+    #encLayer5 = tf.keras.layers.Dense(75,\
+    #                                  kernel_regularizer=tf.keras.regularizers.l1(0.01),\
+    #                                  activity_regularizer=tf.keras.regularizers.l2(0.01),\
+    #                                  activation="relu")
+    #encLayer6 = tf.keras.layers.Dense(200, activation="relu")
+    latentFeatureLayer = tf.keras.layers.Dense(latentDim, activation="relu")
+
+    # Build the actual model
+    model = tf.keras.models.Sequential([inputLayer,\
+                                        encLayer1,\
+                                        encLayer2,\
+                                        #encLayer3,\
+                                        #encLayer4,\
+                                        encFlatten,\
+                                        encDropout,\
+                                        #encLayer5,\
+                                        #encLayer6,\
+                                        latentFeatureLayer])
+
+    return model
+
+
+def BuildDecoder(latentDim, imageWidth, imageHeight):
+    colorChannels=3
+
+    upScale1 = 2
+    upScale2 = 1
+    subWidth1 = int(imageWidth / (upScale1*upScale2))
+    subHeight1 = int(imageHeight / (upScale1*upScale2))
+    subWidth2 = int(imageWidth/upScale2)
+    subHeight2 = int(imageHeight/upScale2)
+
+    latentInputLayer = tf.keras.layers.Input(shape=(latentDim,))
+    decLayer1 = tf.keras.layers.Dense(colorChannels*subWidth1*subHeight1,\
+                                      #kernel_regularizer=tf.keras.regularizers.l1(0.01),\
+                                      #activity_regularizer=tf.keras.regularizers.l2(0.01),\
+                                      activation="relu")
+    decDropout = tf.keras.layers.Dropout(0.01)
+    decLayer2 = tf.keras.layers.Reshape( (subWidth1, subHeight1, colorChannels) )
+    decLayer3 = tf.keras.layers.UpSampling3D( (upScale1, upScale1, 1) )
+    outputLayer = tf.keras.layers.Conv2D(colorChannels, (4, 4), padding="same",\
+                                      kernel_regularizer=tf.keras.regularizers.l1(0.01),\
+                                      #activity_regularizer=tf.keras.regularizers.l2(0.01),\
+                                      activation="sigmoid")
+    #decLayer5 = tf.keras.layers.UpSampling3D( (upScale2, upScale2, 1) )
+    #outputLayer = tf.keras.layers.Conv2D(colorChannels, (3, 3), padding="same", activation="sigmoid")
+
+    # Build the actual model
+    model = tf.keras.models.Sequential([latentInputLayer,\
+                                        decLayer1,\
+                                        decDropout,\
+                                        decLayer2,\
+                                        decLayer3,\
+                                        #decLayer4,\
+                                        #decLayer5,\
+                                        outputLayer])
+
+    return model
+
+latentDim = 50
 
 ### 1) Get the Data
 
-image = tf.image.decode_png(tf.io.read_file("../../data/rpw-pic.png"), channels=3)
-images = tf.reshape(image, (1,630,630,3))
-trainX = tf.cast(images, 'float32') / 255.0
+# Read the first image and make a training dataset for it, call it "A"
+imageA = tf.image.decode_png(tf.io.read_file("../../data/rpw-400x400.png"), channels=3)
+print(tf.shape(imageA))
+
+(width, height, _) = tf.shape(imageA)
+print("Image A is: ", width, "by", height)
+imagesA = tf.reshape( [imageA], (1,width,height,3))
+trainA = tf.cast(imagesA, 'float32') / 255.0
+
+# Read the second image and make a training set for it, call it "B"
+imageB = tf.image.decode_png(tf.io.read_file("../../data/mattsmith-400x400.png"), channels=3)
+imagesB = tf.reshape( [imageB], (1,width,height,3))
+trainB = tf.cast(imagesB, 'float32') / 255.0
+(widthB, heightB, _) = tf.shape(imageB)
+print("Image B is: ", widthB, "by", heightB)
+
+# Assume A and B are the same dimesions for this silly, simple example
 
 # We actually don't care at all about the labels (Y); we wont' use them
 
 
-## How many dimensions should the latent attribute vector be?
-latentDim = 200
-
-
-### 2a) Build Encoder (alone) representation
-
-# Create the encoder layers
-inputLayer = tf.keras.layers.Input(shape=(630, 630, 3))
-encLayer1 = tf.keras.layers.Conv2D(100, (3, 3), activation="relu")
-encLayer2 = tf.keras.layers.MaxPooling2D((5, 5))
-encFlatten = tf.keras.layers.Flatten()
-encLayer3 = tf.keras.layers.Dense(100, activation="relu")
-latentFeatureLayer = tf.keras.layers.Dense(latentDim, activation="relu")
-
-# Build the actual model
-encoderModel = tf.keras.models.Sequential([inputLayer,\
-                                           encLayer1,\
-                                           encLayer2,\
-                                           encFlatten,\
-                                           #encLayer3,\
-                                           latentFeatureLayer])
+### 2a) Build Encoder (shared) representation
+encoderModel = BuildEncoder(latentDim, width, height)
 print()
 print("ENCODER MODEL:")
 print(encoderModel.summary())
 
 
-### 2b) Build the decoder (alone) representation
+### 2b) Build the two decoder representations
+decoderModelA = BuildDecoder(latentDim, width, height)
+decoderModelB = BuildDecoder(latentDim, width, height)
 
-# Create the decoder layers
-latentInputLayer = tf.keras.layers.Input(shape=(latentDim,))
-decLayer1 = tf.keras.layers.Dense(210*210*3, activation="relu")
-decLayer2 = tf.keras.layers.Reshape( (210, 210, 3))
-decLayer3 = tf.keras.layers.UpSampling3D( (3, 3, 1))
-outputLayer = tf.keras.layers.Conv2D(3, (4, 4), padding="same", activation="sigmoid")
-
-
-# Build the actual model
-decoderModel = tf.keras.models.Sequential([latentInputLayer,\
-                                           decLayer1,\
-                                           decLayer2,\
-                                           decLayer3,\
-                                           outputLayer])
-
-# Let's see what the model looks like:
+# Let's see what the A model looks like (B should be the same)
 print()
-print("DECODER MODEL:")
-print(decoderModel.summary())
+print("DECODER A MODEL:")
+print(decoderModelA.summary())
 
-### 2b) Build the whole autoencoder.  You must re-use the layers created above
-autoencoderModel = tf.keras.models.Sequential([inputLayer,\
-                                               encLayer1,\
-                                               encLayer2,\
-                                               encFlatten,\
-                                               #encLayer3,\
-                                               latentFeatureLayer,\
-                                               decLayer1,\
-                                               decLayer2,\
-                                               decLayer3,\
-                                               outputLayer])
+
+### 2b) Build the autoencoders for both:  Use the shared encoder in
+###     both cases, but separate decoders
+layersList = [ tf.keras.layers.Input(shape=(width, height, 3)) ]
+layersList.extend( encoderModel.layers )   # Add all encoder layers
+layersList.extend( decoderModelA.layers )  # Add all decoderA layers but the input
+autoencoderModelA = tf.keras.models.Sequential(layersList)
+
+layersList = [ tf.keras.layers.Input(shape=(width, height, 3)) ]
+layersList.extend( encoderModel.layers )   # Add all encoder layers
+layersList.extend( decoderModelB.layers )  # Add all decoderB layers but the input
+autoencoderModelB = tf.keras.models.Sequential(layersList)
 
 print()
-print("FULL AUTOECODER MODEL:")
-print(autoencoderModel.summary())
+print("FULL AUTOECODER A MODEL:")
+print(autoencoderModelA.summary())
 print()
 
 
-print("Shape of training set: ", tf.shape(trainX))
-print("Shape of expected input: ", autoencoderModel.layers[0].input_shape)
-print()
-
-### 3) Train the Model
+### 3) Train the Models
 
 # Set the model for training
-#  * Use AdaM for optimization
-#  * Compute loss using binary cross entropy
+#  * Use the ADAptive Momementum method for optimization
+#  * Compute loss using mean absolute error
 #  * Report the performance during learning using accuracy
-autoencoderModel.compile( optimizer="Adam", loss="MAE", metrics=['accuracy'])
+autoencoderModelA.compile( optimizer="Adam", loss="MAE", metrics=['accuracy'])
+autoencoderModelB.compile( optimizer="Adam", loss="MAE", metrics=['accuracy'])
 
-# Perform the induction.  This looks a little weird ... so:
-#  a) We are training the network to reproduce its own input.  Thus
-#     each example is also its own truth
-#  b) We're apply validation, but we may as use the test set because
-#     the test set isn't needed for unspervised learning
-trainingHistory = autoencoderModel.fit(trainX, trainX, epochs=200)
+# Perform the induction by alternative between the two networks.
+# Each pass, the shared encoder is updated, as is the specific decoder
+for epochIdx in range(300):
+    print("Overall Epoch:", epochIdx*3)
+    autoencoderModelA.fit(trainA, trainA, epochs=3)
+    autoencoderModelB.fit(trainB, trainB, epochs=3)
 
+#encoderModel.save("encoder.h5")
+#decoderModelA.save("decoderA.h5")
+#decoderModelB.save("decoderB.h5")
 
-### 4) Let's Take a Look
+#encoderModel = tf.keras.models.load_model("encoder.h5")
+#decoderModelA = tf.keras.models.load_model("decoderA.h5")
+#decoderModelB = tf.keras.models.load_model("decoderB.h5")
 
+### 6) The Ol'Switcheroo
+print()
+print("Fake!!")
 
-# Let's just see what it does on the first test example
-rebuiltExamples = autoencoderModel(trainX)
-#originalImage = tf.reshape(testX[0], (28, 28))
-#rebuiltImage  = tf.reshape(rebuiltExamples[0], (28, 28))
+# Use image A's newly learned decoder, but feed it the latent image for image B,
+# then vice-versa
+latentImageA = encoderModel(trainA)
+latentImageB = encoderModel(trainB)
 
-# Use Matplotlib to produce a PDF
-import matplotlib.pyplot as plt
-
-print("Pixel (315,315) in image 1:", rebuiltExamples[0][314][314])
+rebuiltImageMe = decoderModelA(latentImageA)
+rebuiltImageMatt = decoderModelA(latentImageB)
+rebuiltExampleAB = decoderModelA(latentImageB)
+rebuiltExampleBA = decoderModelB(latentImageA)
 
 # Save the Test set image 1, original
-plotting.SaveImage(tf.saturate_cast(255.0*rebuiltExamples[0], dtype = tf.uint8),\
-                   "../../data/rpw-rebuilt.png")
+plotting.SaveImage(tf.saturate_cast(255.0*rebuiltImageMe[0], dtype = tf.uint8),\
+                   "../../data/nofakeAA.png")
+plotting.SaveImage(tf.saturate_cast(255.0*rebuiltImageMatt[0], dtype = tf.uint8),\
+                   "../../data/nofakeBB.png")
+plotting.SaveImage(tf.saturate_cast(255.0*rebuiltExampleAB[0], dtype = tf.uint8),\
+                   "../../data/deepfakeAB.png")
+plotting.SaveImage(tf.saturate_cast(255.0*rebuiltExampleBA[0], dtype = tf.uint8),\
+                   "../../data/deepfakeBA.png")
+#plotting.SaveImage(tf.saturate_cast(255.0*rebuiltExamples[1], dtype = tf.uint8),\
+#                   "../../data/mattsmith-rebuilt.png")
